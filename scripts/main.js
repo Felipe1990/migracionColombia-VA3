d8b0611af84254ec3a5dd0f21c592dc1c223a942
Felipe <realagape@gmail.com>
2018-09-24 11:08:24 -0500

    first working version

--- scripts/main.js
+++ scripts/main.js
@@ -4,24 +4,14 @@ var generalWidth=960,
     width = generalWidth - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
+var y = d3.scaleBand()
+    .range([0, height], 0.1);
+
+var x = d3.scaleLinear()
+    .range([0, width], 0.1);
+
 var bodyChange=d3.select("body")
   .style("width", generalWidth+"px")
-
-var x = d3.scale.linear()
-    .range([0, width]);
-
-var y = d3.scale.ordinal()
-    .rangeRoundBands([0, height], 0.1);
-
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .tickSize(0)
-    .tickPadding(6);
 
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -29,53 +19,38 @@ var svg = d3.select("body").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var tipinfo = d3.tip()
-  .html(function(d) {
-    return "<strong>Nota:</strong> <span style='color:black'>" + (Math.round((+d.nota+3)*10)/10) + "</span>";
-  })
-  .direction(d=>{
-    if (d.nota>0) return 'w'
-    if (d.nota<=0) return 'e'
 
-  })
-  .offset(d=>{
-    if (d.nota>0) return [0, -20]
-    if (d.nota<=0) return [0,10]
-  });
-
-svg.call(tipinfo);
-  
-d3.csv("sample_codes.csv", type, function(error, data) {
+d3.csv("data/sample_codes.csv")
+  .then((data) => {
+        return data.map((d) => {
+          d.nota = +d.nota;
+          return d;  
+        });
+        })
+  .then((data) => {
 // primer hay que ordenar la base .. duh!!!
   datasor=data.sort((a,b) => d3.descending(a.nota, b.nota));
 
-  x.domain(d3.extent(datasor, function(d) { return d.nota; })).nice();
-  y.domain(datasor.map(function(d) { return d.cod; }));
+  x.domain(d3.extent(datasor, d => d.nota))
+    .nice();
+
+  y.domain(datasor.map(d => d.cod));
 
   svg.selectAll(".bar")
-      .data(datasor)
-    .enter().append("rect")
-      .attr("class", function(d) { return "bar bar--" + (d.nota < 0 ? "negative" : "positive"); })
-      .attr("x", function(d) { return x(Math.min(0, d.nota)); })
-      .attr("y", function(d) { return y(d.cod); })
-      .attr("width", function(d) { return Math.abs(x(d.nota) - x(0)); })
-      .attr("height", y.rangeBand())
-      .on('mouseover', function(d, i){
-        tipinfo.show(d,i)})
-      .on('mouseout', tipinfo.hide);
+        .data(datasor)
+      .enter().append("rect")
+        .attr("class", function(d) { return "bar bar--" + (d.nota < 0 ? "negative" : "positive"); })
+        .attr("x", function(d) { return x(Math.min(0, d.nota)); })
+        .attr("y", function(d) { return y(d.cod); })
+        .attr("width", function(d) { return Math.abs(x(d.nota) - x(0)); })
+        .attr("height", y.bandwidth());
 
   svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+    .call(d3.axisLeft(y))
+    .attr("transform", "translate(" + x(0) + ",0)");
 
   svg.append("g")
-      .attr("class", "y axis")
-      .attr("transform", "translate(" + x(0) + ",0)")
-      .call(yAxis);
+    .call(d3.axisBottom(x))
+    .attr("transform", "translate(0," + height + ")");
+
 });
-
-function type(d) {
-  d.nota = +d.nota;
-  return d;
-}
